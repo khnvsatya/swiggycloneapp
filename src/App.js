76f7f9daa593f0420017816d30d03203c0d84168
satyalearn <88238405+satyalearn@users.noreply.github.com>
2023-08-22 01:03:51 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Body from "./components/Body";
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
-import { UserContext } from "./utils/userContext";
+import { UserContext } from "./utils/UserContext";
 
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
