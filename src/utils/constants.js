099a4574d508e866bbfe1d5569958a089a56e230
satyalearn <88238405+satyalearn@users.noreply.github.com>
2023-08-23 22:08:16 +0530

    re-added files to resolve build errors

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const DATA_URL =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&page_type=DESKTOP_WEB_LISTING";
+
+export const CDN_URL =
+  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://corsproxy.io/?https://cdn.icon-icons.com/icons2/3277/PNG/512/fried_chicken_bucket_chicken_wings_food_icon_208005.png";
+
+export const MENU_URL =
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.416889&lng=78.43867&restaurantId=";
+
+export const HAMBURGER_URL =
+  "https://corsproxy.io/?https://cdn.icon-icons.com/icons2/2596/PNG/512/hamburger_button_menu_icon_155296.png";
